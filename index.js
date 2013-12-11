3a2e533d190a0dde24871246111fddc2413b38f3
James Halliday <mail@substack.net>
2013-12-11 00:09:36 -0800

    skip padding, still errors

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ exports.encrypt = function (pub, opts) {
     if (!opts) opts = {};
     if (typeof pub !== 'string') pub = String(pub);
     if (/^ssh-\w+/.test(pub)) pub = toPEM(pub);
-    var enc = opts.encoding || 'binary';
-    if (enc === 'binary') enc = undefined;
+    var enc = opts.encoding || 'buffer';
+    if (enc === 'buffer') enc = undefined;
     
     var pubkey = ursa.createPublicKey(pub);
     var bufsize = pubkey.getModulus().length;
-    var blocks = new BlockStream(bufsize - 42);
+    var blocks = new BlockStream(bufsize - 42, { nopad: true });
     
     return combine(blocks, through(function (buf, e, next) {
         this.push(pubkey.encrypt(buf, undefined, enc));
@@ -26,7 +26,7 @@ exports.decrypt = function (priv, opts) {
     if (!opts) opts = {};
     if (typeof priv !== 'string') priv = String(priv);
     if (/^ssh-\w+/.test(priv)) priv = toPEM(priv);
-    var enc = opts.encoding || 'binary';
+    var enc = opts.encoding || 'buffer';
     
     var privkey = ursa.createPrivateKey(priv);
     var bufsize = privkey.getModulus().length;
@@ -37,7 +37,7 @@ exports.decrypt = function (priv, opts) {
         next();
     });
     
-    if (enc === 'binary') {
+    if (enc === 'buffer') {
         return combine(blocks, decrypt);
     }
     
