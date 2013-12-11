97d616c2bcc1ad4989cbdfbe07eb65dfd53b433b
James Halliday <mail@substack.net>
2013-12-10 22:26:33 -0800

    use a duplex stream instead

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 var ursa = require('ursa');
 var toPEM = require('http-signature').sshKeyToPEM;
-var Transform = require('stream').Transform;
+var Duplex = require('stream').Duplex;
 var inherits = require('inherits');
 
 exports.encrypt = Encrypt;
 exports.decrypt = Decrypt;
 
-function Encrypt (pub, opts) {
-    if (!(this instanceof Encrypt)) return new Encrypt(pub, opts);
-    if (!opts) opts = {};
+inherits(Encrypt, Duplex);
+inherits(Decrypt, Duplex);
+
+function Encrypt (pub) {
+    if (!(this instanceof Encrypt)) return new Encrypt(pub);
+    Duplex.call(this);
+    
     if (typeof pub !== 'string') pub = String(pub);
-    if (!/^ssh-\w+/.test(pub)) pub = toPEM(pub);
+    if (/^ssh-\w+/.test(pub)) pub = toPEM(pub);
     this.pubkey = ursa.createPublicKey(pub);
+    this.bufsize = this.pubkey.getModulus().length;
+    this._buffer = [];
+    this._pending = 0;
 }
 
-Encrypt.prototype._transform = function (buf) {
+Encrypt.prototype._write = function (buf, enc, next) {
+    console.log(buf);
+    if (this._pending) {
+    }
 };
 
-Encrypt.prototype._flush = function () {
+Encrypt.prototype._read = function (size) {
 };
 
 function Decrypt (priv) {
     if (!(this instanceof Encrypt)) return new Encrypt(priv);
+    Duplex.call(this);
+    
     if (typeof pub !== 'string') pub = String(pub);
-    if (!/^ssh-\w+/.test(pub)) pub = toPEM(pub);
+    if (/^ssh-\w+/.test(pub)) pub = toPEM(pub);
     this.privkey = ursa.createPrivateKey(pub);
 }
 
@@ -32,5 +44,5 @@ Decrypt.prototype._transform = function () {
 
 Decrypt.prototype._flush = function () {
     var privkey = ursa.createPrivateKey(priv);
-    console.log(privkey.decrypt(enc).toString('utf8'));
+    //console.log(privkey.decrypt(enc).toString('utf8'));
 };
