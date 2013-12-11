5bea101ff286761a09aad6f6447e8264aab057c2
James Halliday <mail@substack.net>
2013-12-10 22:45:47 -0800

    just use block-stream and stream-combiner

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
 var ursa = require('ursa');
 var toPEM = require('http-signature').sshKeyToPEM;
-var Duplex = require('stream').Duplex;
 var inherits = require('inherits');
+var BlockStream = require('block-stream');
+var combine = require('stream-combiner');
+var through = require('through2');
 
-exports.encrypt = Encrypt;
-exports.decrypt = Decrypt;
-
-inherits(Encrypt, Duplex);
-inherits(Decrypt, Duplex);
-
-function Encrypt (pub) {
-    if (!(this instanceof Encrypt)) return new Encrypt(pub);
-    Duplex.call(this);
-    
+exports.encrypt = function (pub, opts) {
+    if (!opts) opts = {};
     if (typeof pub !== 'string') pub = String(pub);
     if (/^ssh-\w+/.test(pub)) pub = toPEM(pub);
-    this.pubkey = ursa.createPublicKey(pub);
-    this.bufsize = this.pubkey.getModulus().length;
-    this._buffer = [];
-    this._pending = 0;
-}
-
-Encrypt.prototype._write = function (buf, enc, next) {
-    console.log(buf);
-    if (this._pending) {
-    }
-};
-
-Encrypt.prototype._read = function (size) {
-};
-
-function Decrypt (priv) {
-    if (!(this instanceof Encrypt)) return new Encrypt(priv);
-    Duplex.call(this);
+    var enc = opts.encoding || 'base64';
     
-    if (typeof pub !== 'string') pub = String(pub);
-    if (/^ssh-\w+/.test(pub)) pub = toPEM(pub);
-    this.privkey = ursa.createPrivateKey(pub);
-}
-
-Decrypt.prototype._transform = function () {
-};
-
-Decrypt.prototype._flush = function () {
-    var privkey = ursa.createPrivateKey(priv);
-    //console.log(privkey.decrypt(enc).toString('utf8'));
+    var pubkey = ursa.createPublicKey(pub);
+    var bufsize = pubkey.getModulus().length - 42;
+    
+    return combine(new BlockStream(bufsize), through(function (buf) {
+        this.push(pubkey.encrypt(buf, undefined, enc));
+    }));
 };
